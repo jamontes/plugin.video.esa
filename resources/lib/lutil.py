9b85362ab4ae58befebf3a631206fd6bc4bb82cb
jamontes <jamontes@users.noreply.github.com>
2021-05-18 01:04:54 +0200

    Code updated for matrix onwards.

--- resources/lib/lutil.py
+++ resources/lib/lutil.py
@@ -1,33 +1,29 @@
-# _*_ coding: utf-8 _*_
-
 '''
-   lutil: library functions for XBMC video plugins.
+   lutil: library functions for KODI video plugins.
    Copyright (C) 2013 José Antonio Montes (jamontes)
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   SPDX-License-Identifier: GPL-3.0-or-later
+   See  https://spdx.org/licenses/ and the LICENSE.txt file
+   included for more info about the license.
 
-   Description:
-   These funtions are called from the main plugin module, aimed to ease and simplify the plugin development process.
-   Release 0.1.3
+   These funtions are called from the main plugin module,
+   aimed to ease and simplify the plugin development process.
+   Release 1.3.0
 '''
 
 # First of all We must import all the libraries used for plugin development.
-import sys, re, urllib, urllib2, json, os
+import sys, re, os
+from urllib.parse import urlencode, quote_plus, unquote_plus
+from urllib.request import urlopen, Request
 import xbmcplugin, xbmcaddon, xbmcgui, xbmcaddon, xbmc
 
 debug_enable = False # The debug logs are disabled by default.
 fanart_file = "" # Initialize the global var for the fanart file location.
+
+
+# This function returns the plugin handle to main module.
+def get_plugin_handle():
+    return int(sys.argv[1])
 
 
 # This function returns the plugin settings object to main module.
@@ -35,7 +31,7 @@ def get_plugin_settings(plugin_id=""):
     return xbmcaddon.Addon(id=plugin_id)
 
 
-# This function returns the current system language.    
+# This function returns the current system language.
 def get_system_language():
     return xbmc.getLanguage()
 
@@ -54,19 +50,19 @@ def set_fanart_file(root_path=""):
     xbmcplugin.setPluginFanart(int(sys.argv[1]), fanart_file)
 
 
-# This function logs the messages into the main XBMC log file. Called from main plugin module.
+# This function logs the messages into the main KODI log file. Called from main plugin module.
 def log(message):
     if debug_enable:
-        xbmc.log("%s" % message, level=xbmc.LOGNOTICE)
+        xbmc.log("%s" % message, level=xbmc.LOGINFO)
 
 
-# This function logs the messages into the main XBMC log file. Called from the libraries module by other functions.
+# This function logs the messages into the main KODI log file. Called from the libraries module by other functions.
 def _log(message):
     if debug_enable:
-        xbmc.log("lutils.%s" % message, level=xbmc.LOGNOTICE)
+        xbmc.log("lutils.%s" % message, level=xbmc.LOGINFO)
 
 
-# This function gets all the parameters passed to the plugin from XBMC API and retuns a dictionary.
+# This function gets all the parameters passed to the plugin from KODI API and retuns a dictionary.
 # Example:
 # plugin://plugin.video.atactv/?parametro1=valor1&parametro2=valor2&parametro3
 def get_plugin_parms():
@@ -74,7 +70,7 @@ def get_plugin_parms():
     _log("get_plugin_parms " + str(params))
 
     pattern_params  = re.compile('[?&]([^=&]+)=?([^&]*)')
-    options = dict((parameter, urllib.unquote_plus(value)) for (parameter, value) in pattern_params.findall(params))
+    options = dict((parameter, unquote_plus(value)) for (parameter, value) in pattern_params.findall(params))
 
     _log("get_plugin_parms " + repr(options))
     return options
@@ -83,19 +79,13 @@ def get_plugin_parms():
 # This function returns the URL decoded.
 def get_url_decoded(url):
     _log('get_url_decoded URL: "%s"' % url)
-    return urllib.unquote_plus(url)
+    return unquote_plus(url)
 
 
 # This function returns the URL encoded.
 def get_url_encoded(url):
     _log('get_url_encoded URL: "%s"' % url)
-    return urllib.quote_plus(url)
-
-
-# This function sets the view mode into the video list.
-def set_view_mode(viewid):
-    _log("set_view_mode mode: " + viewid)
-    xbmc.executebuiltin('Container.SetViewMode('+viewid+')')
+    return quote_plus(url)
 
 
 # This function sets the video contents for the video list.
@@ -112,7 +102,7 @@ def set_plugin_category(pluginhandle, genre=''):
 # This function gets an input text from the keyboard.
 def get_keyboard_text(prompt):
     _log('get_keyboard_text prompt: "%s"' % prompt)
-    
+
     keyboard = xbmc.Keyboard('', prompt)
     keyboard.doModal()
     if keyboard.isConfirmed() and keyboard.getText():
@@ -126,11 +116,12 @@ def get_keyboard_text(prompt):
 def carga_web(url):
     _log("carga_web " + url)
 
-    MiReq = urllib2.Request(url) # We use the Request method because we need to add a header into the HTTP GET to the web site.
+    MiReq = Request(url) # We use the Request method because we need to add a header into the HTTP GET to the web site.
     # We have to tell the web site we are using a real browser.
     MiReq.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0') # This is a true Firefox header.
-    MiConex = urllib2.urlopen(MiReq) # We open the HTTP connection to the URL.
-    MiHTML = MiConex.read() # We load all the HTML contents from the web page and store it into a var.
+    MiConex = urlopen(MiReq) # We open the HTTP connection to the URL.
+    encoding = MiConex.info().get_param('charset', 'utf8')
+    MiHTML = MiConex.read().decode(encoding, 'ignore') # We load all the HTML contents from the web page and store it into a var.
     MiConex.close() # We close the HTTP connection as we have all the info required.
 
     return MiHTML
@@ -141,7 +132,7 @@ def find_multiple(text,pattern):
     _log("find_multiple pattern=" + pattern)
 
     pat_url_par = re.compile(pattern, re.DOTALL)
-   
+
     return pat_url_par.findall(text)
 
 
@@ -156,30 +147,30 @@ def find_first(text,pattern):
         return ""
 
 
-# This function adds a directory entry into the XBMC GUI throught the API
+# This function adds a directory entry into the KODI GUI throught the API
 def addDir(action = "", title = "", url = "", genre = "", reset_cache = "no"):
     _log('addDir action = "%s" url = "%s" reset_cache = "%s"' % (action, url, reset_cache))
 
-    dir_url = '%s?action=%s&reset_cache=%s&url=%s&genre=%s' % (sys.argv[0], action, reset_cache, urllib.quote_plus(url), urllib.quote_plus(genre))
-    dir_item = xbmcgui.ListItem(title, iconImage = "DefaultFolder.png", thumbnailImage = '')
+    dir_url = '%s?action=%s&reset_cache=%s&url=%s&genre=%s' % (sys.argv[0], action, reset_cache, quote_plus(url), quote_plus(genre))
+    dir_item = xbmcgui.ListItem(title)
     dir_item.setInfo(type = "Video", infoLabels = {"Title": title, "Genre": genre})
-    dir_item.setProperty('Fanart_Image', fanart_file)
+    dir_item.setArt({ 'fanart': fanart_file })
     return xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = dir_url, listitem = dir_item, isFolder = True)
 
 
-# This function adds a video link entry into the XBMC GUI throught the API
+# This function adds a video link entry into the KODI GUI throught the API
 def addLink(action = "", title = "", url = "", thumbnail = "", video_info = {}, show_fanart = False):
     _log("addLink action = [" + action + "] title = [" + title + "] url = [" + url + "] thumbnail = [" + thumbnail + "]")
 
-    link_url = '%s?action=%s&url=%s' % (sys.argv[0], action, urllib.quote_plus(url))
-    link_item = xbmcgui.ListItem(title, iconImage = "DefaultVideo.png", thumbnailImage = thumbnail)
+    link_url = '%s?action=%s&url=%s' % (sys.argv[0], action, quote_plus(url))
+    link_item = xbmcgui.ListItem(title)
     video_info['Title'] = title
     link_item.setInfo(type = "Video", infoLabels = video_info)
     link_item.setProperty('IsPlayable', 'true')
-    if show_fanart:
-        link_item.setProperty('Fanart_Image', thumbnail)
-    else:
-        link_item.setProperty('Fanart_Image', fanart_file)
+    link_item.setArt({
+        'thumb': thumbnail,
+        'fanart': thumbnail if show_fanart else fanart_file,
+        })
     return xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = link_url, listitem = link_item, isFolder = False)
 
 
@@ -189,10 +180,10 @@ def close_dir(pluginhandle, succeeded=True, updateListing=False, cacheToDisc=Tru
     xbmcplugin.endOfDirectory(pluginhandle, succeeded=succeeded, updateListing=updateListing, cacheToDisc=cacheToDisc)
 
 
-# This funtion shows a popup window with a notices message through the XBMC GUI during 5 secs.
+# This funtion shows a popup window with a notices message through the KODI GUI during 6 secs.
 def showWarning(message):
     _log("showWarning message: %s" % message)
-    xbmc.executebuiltin('XBMC.Notification(Info:,' + message + '!,6000)')
+    xbmcgui.Dialog().notification('Info:', message, time=6000)
 
 
 # This function plays the video file pointed by the URL passed as argument.
